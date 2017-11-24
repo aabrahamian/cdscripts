826a25f3a0e926d92f757f441fe1b406bb238cf7
aabrahamian <abrahamian@looker.com>
2017-11-24 11:23:11 -0800

    echo random number generated

--- flakey.sh
+++ flakey.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 echo "flakey job"
-TO_FLAKE_OR_NOT_TO_FLAKE=$(( RANDOM % 2 == 0 ))
+RANDOM_NUMBER=$RANDOM
+echo "random number: $RANDOM_NUMBER"
+TO_FLAKE_OR_NOT_TO_FLAKE=$(( RANDOM_NUMBER % 2 == 0 ))
 echo "FLAKE=($TO_FLAKE_OR_NOT_TO_FLAKE)"
 exit $TO_FLAKE_OR_NOT_TO_FLAKE
--- sleep_rand.sh
+++ sleep_rand.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 echo "test with random sleep time (2-30 seconds) here"
-TIME_TO_SLEEP=$(( RANDOM % 29 + 2 ))
+RANDOM_NUMBER=$RANDOM
+echo "random number: $RANDOM_NUMBER"
+TIME_TO_SLEEP=$(( RANDOM_NUMBER % 29 + 2 ))
 echo "sleeping for $TIME_TO_SLEEP seconds"
 sleep $TIME_TO_SLEEP
 echo "done"
